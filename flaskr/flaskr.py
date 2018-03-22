d735115b3b5bfa6db177f3917162491394ca90e8
cicolord <cicolord@outlook.com>
2018-03-22 22:39:53 +0800

    Render failed

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -6,24 +6,22 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from jinja2 import Environment, PackageLoader, select_autoescape
 app = Flask('flaskr')
 app.config['SECRET_KEY'] = 'hard to guess string'
-# @app.route("/",methods=['GET','POST'])
-# def index():
-#     form = MockCreate()
-#     text= form['text']
-#     demoCSS=url_for("static",filename="stylesheet.css")
-#     yuanwen = request.form['yuanwen']
-#     print(yuanwen)
-#     return render_template("demo.html",demoCSS=demoCSS)
 
+env = Environment(
+    loader=PackageLoader('flaskr', 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+def redirect_url(default='index'):
+    return request.args.get('next') or \
+           request.referrer or \
+           url_for(default)
 
 @app.route("/")
 def index():
-    fanyi1 = "fanyi1"
-    fanyi2 = "fanyi2"
-    zidian = "zidian"
-    return render_template("base.html",fanyi1=fanyi1,fanyi2=fanyi2,zidian=zidian)
+    return render_template("base.html")
 
 @app.route("/",methods=['POST','GET'])
 def my_form_post():
@@ -33,15 +31,19 @@ def my_form_post():
         #处理函数
         # return text
         print(text)
-        fanyi1="fanyi1"
-        fanyi2 = "fanyi2"
-        zidian="zidian"
+        fanyi1="fanyi1"+text
+        fanyi2 = "fanyi2"+text
+        zidian="zidian"+text
         templateData = {
             'yuanwen':text,
             'fanyi1':fanyi1,
             'fanyi2':fanyi2,
             'zidian':zidian
         }
-        return render_template("base.html",**templateData)
+        # render_template("base.html",**templateData)
+        # template = env.get_template('base.html')
+        # template.render(yuanwen=text, fanyi1 = fanyi1, fanyi2 = fanyi2, zidian = zidian)
+        return render_template("base.html",yuanwen=text, fanyi1 = fanyi1, fanyi2 = fanyi2, zidian = zidian)
+        # return redirect(redirect_url())
 if __name__ == '__main__':
     app.run(debug=True)
